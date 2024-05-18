6f5896593960d33ed5f299cb9dafef18aa44a485
Fritzgerrad <fritzgerrad.so@gmail.com>
2024-05-18 15:20:00 +0100

    Initailize the springboot app for the backend

---
+++ src/main/java/com/frz/inkmason/InkmasonBackendApplication.java
@@ -0,0 +1,13 @@
+package com.frz.inkmason;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InkmasonBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InkmasonBackendApplication.class, args);
+	}
+
+}
