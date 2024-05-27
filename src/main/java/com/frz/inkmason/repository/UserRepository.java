19f3ac7c8867b6d55a7e4342de8ffb3ccfe4cf8c
Fritzgerrad <fritzgerrad.so@gmail.com>
2024-05-27 07:56:45 +0100

    Added the UserDto, UserService and UserRepository classes

---
+++ src/main/java/com/frz/inkmason/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.frz.inkmason.repository;
+
+import com.frz.inkmason.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional <User> findUserByEmail(String email);
+}
